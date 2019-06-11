1fdf7da5daa4c5d057687872089caf2413a07870
plupinska <patrycjalupinska@gmail.com>
2019-06-11 08:36:14 -0400

    refactor code with an oo approach

--- src/pizzaProblem.js
+++ src/pizzaProblem.js
@@ -1,62 +1,97 @@
+// add path to employee
+// factor out summarization
 
-const pizzaDeliveriesAlone = directions => {
-  const uniqPizzaHouses = new Set();
-  let mariasLocation = [0,0];
-  const addToSet = addCoordinatesToSet(uniqPizzaHouses);
-  addToSet(mariasLocation);
-  let i = 0;
-
-  while (i < directions.length) {
-    let direction = directions[i];
-    let mariasNextLocation = calcCoordinates(mariasLocation, direction);
-    addToSet(mariasNextLocation);
-    i++;
+class Employee {
+  constructor(name) {
+    this.name = name;
+    this.path = "";
   }
 
-  return uniqPizzaHouses.size;
+  addToPath(direction) {
+    this.path += direction;
+  }
+}
+
+class DeliveryDelegator {
+  constructor(directions, employees) {
+    this.directions = directions;
+    this.employees = employees;
+  }
+
+  delegate() {
+    for (let i  = 0; i < this.employees.length; i++) {
+      let j = i;
+      const currentEmployee = this.employees[i];
+      while (j < this.directions.length) {
+        currentEmployee.addToPath(this.directions[j]);
+        j += this.employees.length;
+      }
+    }
+  }
+}
+
+class Summarizer {
+  constructor(employees) {
+    this.employees = employees;
+  }
+
+  uniqDeliveries() {
+    const homes = new Set();
+    let currentLocation = [0,0];
+    homes.add(JSON.stringify(currentLocation));
+
+    this.employees.forEach(employee => {
+      let deliveryPath = employee.path;
+      for (let i = 0; i < deliveryPath.length; i++) {
+        const direction = deliveryPath[i];
+        currentLocation = this.calculateLocation(direction, currentLocation);
+        homes.add(JSON.stringify(currentLocation));
+      }
+
+      currentLocation = [0,0];
+    });
+
+    return homes.size;
+  }
+
+  calculateLocation(direction, location) {
+    switch (direction) {
+      case "^":
+        location[1] += 1;
+        return location;
+      case "v":
+        location[1] -= 1;
+        return location;
+      case "<":
+        location[0] -= 1;
+        return location;
+      case ">":
+        location[0] += 1;
+        return location;
+      default:
+        throw "You have entered an incorrect parameter!";
+    }
+  }
+}
+
+const employeeDeliverySummary = (directions, employees) => {
+  const deliveries = new DeliveryDelegator(directions, employees);
+  deliveries.delegate();
+  const summarizer = new Summarizer(employees);
+  return summarizer.uniqDeliveries();
+};
+
+const pizzaDeliveriesAlone = directions => {
+  const maria = new Employee('Maria');
+  const employees = [maria];
+  return employeeDeliverySummary(directions, employees);
 };
 
 const pizzaDeliveriesWithFriend = directions => {
-  const uniqPizzaHouses = new Set();
-  let mariasLocation = [0,0];
-  let clovisLocation = [0,0];
-  const addToSet = addCoordinatesToSet(uniqPizzaHouses);
-  addToSet(mariasLocation);
-  let i = 0;
-  let j = 1;
-
-
-  while (i < directions.length && j < directions.length) {
-    mariasLocation = calcCoordinates(mariasLocation, directions[i]);
-    clovisLocation = calcCoordinates(clovisLocation, directions[j]);
-    addToSet(mariasLocation);
-    addToSet(clovisLocation);
-    i = j + 1;
-    j += 2;
-  }
-
-  return uniqPizzaHouses.size;
-};
-
-const addCoordinatesToSet = set => coordinates => set.add(JSON.stringify(coordinates));
-
-const calcCoordinates = (location, nextStep) => {
-  switch (nextStep) {
-    case "^":
-      location[1] += 1;
-      return location;
-    case "v":
-      location[1] -= 1;
-      return location;
-    case "<":
-      location[0] -= 1;
-      return location;
-    case ">":
-      location[0] += 1;
-      return location;
-    default:
-      throw "You have entered an incorrect parameter!";
-  }
+  const maria = new Employee('Maria');
+  const clovis = new Employee('Clovis');
+  const employees = [maria, clovis];
+  return employeeDeliverySummary(directions, employees);
 };
 
 module.exports = {
